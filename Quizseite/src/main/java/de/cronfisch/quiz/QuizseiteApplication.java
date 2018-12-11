294829c2eb5787ba38c3eb78159efe389e02dbc7
Marcel <Marcel@M-Laptop.fritz.box>
2018-12-11 19:21:13 +0100

    First Commit
    Uploading all files

---
+++ Quizseite/src/main/java/de/cronfisch/quiz/QuizseiteApplication.java
@@ -0,0 +1,12 @@
+package de.cronfisch.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizseiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizseiteApplication.class, args);
+	}
+}
---
+++ Quizseite/src/test/java/de/cronfisch/quiz/QuizseiteApplicationTests.java
@@ -0,0 +1,16 @@
+package de.cronfisch.quiz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizseiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
