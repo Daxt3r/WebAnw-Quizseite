59bba0fbb48526b80ff3b55e7c963a8aa4eff38d
Tim <Tim@192.168.0.206>
2019-01-20 17:19:49 +0100

    tabelle, liste, javascripts und css

--- Quizseite/src/main/resources/static/js/javascripts.js
+++ Quizseite/src/main/resources/static/js/javascripts.js
@@ -1,6 +1,18 @@
 function hinweisFunction() {
+	var x = document.getElementById("hinweis");
 	alert("Nach Beendigung vom Quiz werden Name und E-Mail erfordert. Diese können wieder manuell gelöscht werden.");
 	document.getElementById("hinweis").innerHTML = "Hinweis ist gelesen, danke!";
+	x.style.color = "green";
+}
+
+function versteckFunction() {
+	var x = document.getElementById("verstecken");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		}
+		else {
+			x.style.display = "none";
+		}
 }
 
 function sendPUT() {
